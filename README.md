ab9821b05cf8b83c20555e41025a091105c6b386
Cody Coderson <code@crediblecreative.com>
2020-09-16 20:50:41 -0500

    Added three files, README content...

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # flutter-dart-podcastindex-org-example
 This is an example of how to connect to the podcastindex.org API using Flutter/Dart
+
+    1) Replace the Flutter app's auto-generated lib/main.dart with the new version from this repo
+
+    2) Update the lib/main.dart file with  your API key and API secret
+
+    3) Replace the pubspec.yaml file with the new version from this repo (this handles package dependencies)
+
+    4) Replace the /android/app/src/main/AndroidManifest.xml with the new version from this repo (this contains the permission necessary for the app to access the internet from an Android emulator)
+
+## Potential Issues
+
+    1) The Android emulator has no network access: Add 8.8.8.8 to your machine's list of DNS servers. This is Google's own DNS server, which the app is likely trying to access. Another potential issue is that the app is attempting to run through a proxied setup. Open the emulator's Extended Controls, then click Settings, then click the Proxy tab. Here you can select No Proxy.
+
+    2) The web app thows CORS errors: The only remedy is to have a CORS header sent from the API. As of this writing the API endpoint the example is attempting to access should have CORS enabled, so this should not be a problem.
---
+++ main.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+import 'dart:convert';
+//import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:convert/convert.dart';
+import 'package:crypto/crypto.dart';
+
+Future<PodCastIndex> fetchPodCastIndex() async {
+  var unixTime = (DateTime.now().millisecondsSinceEpoch / 1000).round();
+  String newUnixTime = unixTime.toString();
+  // Change to your API key...
+  var apiKey = "ABC";
+  // Change to your API secret...
+  var apiSecret = "ABC";
+  var firstChunk = utf8.encode(apiKey);
+  var secondChunk = utf8.encode(apiSecret);
+  var thirdChunk = utf8.encode(newUnixTime);
+
+  var output = new AccumulatorSink<Digest>();
+  var input = sha1.startChunkedConversion(output);
+  input.add(firstChunk);
+  input.add(secondChunk);
+  input.add(thirdChunk);
+  input.close();
+  var digest = output.events.single;
+
+  Map<String, String> headers = {
+    "X-Auth-Date": newUnixTime,
+    "X-Auth-Key": apiKey,
+    "Authorization": digest.toString(),
+    "User-Agent": "SomethingAwesome/1.0.1"
+  };
+
+  final response = await http.get(
+      'https://api.podcastindex.org/api/1.0/search/byterm?q=bastiat',
+      headers: headers);
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return PodCastIndex.fromJson(json.decode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class PodCastIndex {
+  final String content;
+
+  PodCastIndex({this.content});
+
+  factory PodCastIndex.fromJson(Map<String, dynamic> json) {
+    debugPrint(json.toString());
+    //return PodCastIndex(foo: json['query']);
+    return PodCastIndex(content: json.toString());
+  }
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<PodCastIndex> futurePodCastIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePodCastIndex = fetchPodCastIndex();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Podcast Listing JSON',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fetch Podcast Listing JSON'),
+        ),
+        body: Center(
+          child: FutureBuilder<PodCastIndex>(
+            future: futurePodCastIndex,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.foo);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
